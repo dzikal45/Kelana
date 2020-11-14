d914ea67a983da0d726cb3e3d88129916f886ca1
dzikal45 <dzikal45@gmail.com>
2020-11-14 15:16:45 +0700

    kelana

---
+++ resources/views/component/header.blade.php
@@ -0,0 +1,13 @@
+<div class="topnav">
+    <div class="container-fluid"></div>
+    <div class="navbar-header">
+        <a href="{{ url('/') }}" class="navbar-brand">Kelana</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+        <li data-toggle="modal"><a href="#"></span> Recommender</a></li>
+        <li data-toggle="modal"><a href="#"></span> Hiburan</a></li>
+        <!-- <li data-toggle="modal" data-target="#mydaftar"><a href="#"></span> Daftar</a></li> -->
+        <li data-toggle="modal" data-target="#mylogin"><a style="cursor: pointer;">Login</a></li>
+    </ul>
+</div>
+</div>
